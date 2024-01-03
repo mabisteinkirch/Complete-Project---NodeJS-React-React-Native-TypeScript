ae83e3c759085581ce2d0e312e14e49758876b2b
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-03 09:52:12 -0300

    Feat: use of the innerHTML function in JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-
-var sobrenome = prompt("Qual é o seu sobrenome?");
-console.log ("Olá, " + nome + " " + sobrenome);
-
-document.write("<h1> Bem vindo " + nome + " ao Projeto</h1>");
-
-document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Image'");
-
 var area = document.getElementById("area");
 
 function entrar (){
     var nome = prompt("Qual é o seu nome?");
+
+    area.innertHTML = "Bem vindo " + nome;
 }
