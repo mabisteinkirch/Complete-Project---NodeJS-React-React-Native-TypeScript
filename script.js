44301a750a1936b0d9390ebbe77297c31b029141
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-03 09:27:12 -0300

    Feat: use of the document.write function in JavaScript to display an image

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ var sobrenome = prompt("Qual é o seu sobrenome?");
 console.log ("Olá, " + nome + " " + sobrenome);
 
 document.write("<h1> Bem vindo " + nome + " ao Projeto</h1>");
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Image'");
