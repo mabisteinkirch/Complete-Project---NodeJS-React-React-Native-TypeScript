4ca79a1896ffcc0369f7a04c3cd047cf0585c789
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-03 09:55:32 -0300

    Feat: use of the conditional in JavaScript

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ var area = document.getElementById("area");
 function entrar (){
     var nome = prompt("Qual é o seu nome?");
 
-    area.innertHTML = "Bem vindo " + nome;
+    if (nome === '' || nome === null){
+        alert ("Campo não preenchido");
+        area.innertHTML = "Clique no botão para acessar"
+    }
+    else {
+        area.innertHTML = "Bem vindo " + nome;
+    }
+
+    
 }
