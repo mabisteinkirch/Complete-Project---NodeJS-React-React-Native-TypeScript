636790f415a657ff458eff520f66e2c546ffd3e2
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-04 11:29:37 -0300

    Feat: using loop-while in JavaScript

--- script.js
+++ script.js
@@ -1,44 +1,6 @@
-var area = document.getElementById("area")
+var x = 0
 
-function entrar(){
-    var nome = prompt("Qual é o seu nome?");
-
-    if (nome === '' || nome === null){
-        alert ("Campo não preenchido");
-        area.innertHTML = "Clique no botão para acessar"
-    }
-    else {
-        area.innerHTML = "Bem vindo " + nome + " ";
-
-        let botao_sair = document.createElement("button");
-        botao_sair.innerText = "Sair da conta";
-        botao_sair.onclick = sair;
-        area.appendChild(botao_sair);
-    }
-
-    
+while (x<10){
+    document.write ("<br> O Valor do X é: " + x);
+    x++;
 }
-
-function sair (){
-    alert ("até mais");
-    area.innerHTML = "Você saiu";
-}
-
-function media (nota1, nota2){
-    var media = (nota1, nota2) / 2;
-
-    if (media >= 7){
-        console.log ("Aluno aprovado com média: " + media)
-    }
-    else if (media < 7){
-        console.log ("Aluno reprovado com média: " + media)
-    }
-}
-
-function aluno (nome, curso){
-    var mensagem = "Seja bem vindo " + nome + " ao curso de " + curso;
-
-    console.log (mensagem);
-}
-
-var lista = ["Mabi", "Lucas", 23];
