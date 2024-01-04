ee962fb50f062ee96a217355f3b28b54dd54989d
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-04 11:00:38 -0300

    Feat: using functions with parameters in JavaScript

--- script.js
+++ script.js
@@ -23,3 +23,20 @@ function sair (){
     alert ("até mais");
     area.innerHTML = "Você saiu";
 }
+
+function media (nota1, nota2){
+    var media = (nota1, nota2) / 2;
+
+    if (media >= 7){
+        console.log ("Aluno aprovado com média: " + media)
+    }
+    else if (media < 7){
+        console.log ("Aluno reprovado com média: " + media)
+    }
+}
+
+function aluno (nome, curso){
+    var mensagem = "Seja bem vindo " + nome + " ao curso de " + curso;
+
+    console.log (mensagem);
+}
