d8b059a79868e9180999169d8c3eaa8bfbef7e71
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-05 14:05:08 -0300

    Feat: setInterval timer function in JavaScript

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
-/*
-Abaixo de 17        - Muito abaixo do peso
-Entre 17 e 18,49    - Abaixo do peso
-Entre 18,5 e 24,99  - Peso normal
-Entre 25 e 29,99    - Acima do peso
-*/
-
-//peso / (altura*altura)
-
-var peso, altura, imc, resultado;
-
-function calcular (event){
-    event.preventDefault();
-
-    peso = document.getElementById('peso').value;
-    altura = document.getElementById('altura').value;
-
-    imc = peso / (altura * altura);
-    resultado = document.getElementById('resultado');
-
-    if (imc < 17){
-        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está muito abaixo do peso';
-    }else if(imc >= 17 && imc < 18.49){
-        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está abaixo do peso';
-    }else if (imc > 18.5 && imc <= 24.99){
-        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está com peso normal';
-    }else if (imc > 25 && imc < 29.99 ){
-        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está acima do peso';
-    }else if(imc >= 30){
-        resultado.innerHTML= '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado obesidade';
-    }
-
-    //limpar o campo depois de calcular
-    document.getElementById('peso').value = '';
-    document.getElementById('altura').value = '';
+function acao (){
+    document.write("Executando... <br/>");
 }
+
+//tempo em milisegundo
+setInterval(acao, 3000)
