048e0d937abe433aaf8169991c96d39e93765a8b
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-14 08:22:21 -0300

    Feat: Spread operator with object in JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 //spread operator
 
+/*
 let primeiros = [1,2,3];
 
 let numeros = [...primeiros, 4,5,10];
 console.log(numeros);
+*/
 
+let pessoa ={
+    nome: "Mabi",
+    cargo: "TI"
+}
+
+let novaPessoa ={
+    ...pessoa,
+    status: "ATIVO",
+    cidade: "Nice / FR"
+}
+
+console.log(novaPessoa);
