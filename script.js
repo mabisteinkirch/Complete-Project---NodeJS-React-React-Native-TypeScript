4c1b21759dc1cfb57a967d45f339c7e44f9f88d3
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-14 11:12:48 -0300

    Feat: Using includes function in JavaScript

--- script.js
+++ script.js
@@ -4,3 +4,9 @@
 let nomes = ["Mabi", "Lucas"];
 
 console.log(nomes.includes("Mabi"));
+
+if(nomes.includes("Mabi")){
+    console.log("Está na lista");
+}else{
+    console.log("Não está na lista")
+}
