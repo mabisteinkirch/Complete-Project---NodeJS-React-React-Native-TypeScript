bf3b57b09306c7e4e309c3a4d9ba2a087f46f07e
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-14 09:48:39 -0300

    Feat: reduce function in JavaScript

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ let lista = ["Mabi", "Lucas", "Nekochan"];
 lista.map((item, index) => {
     console.log("Esse é o item: " + item + " Esse é o index: " + index);
 })
+
+//REDUCE - BUSCA REDUZIR UM ARRAY
+
+let numeros =  [5,3,2];
+// somar todos os numeros
+
+let total = numeros.reduce((acumulador, numero, indice, original)=>{
+    console.log("Esse é o acumulador: " + `${acumulador}`);
+    console.log("Esse é o numero: " + `${numero}`);
+    console.log("Esse é o indice: " + `${indice}`);
+    console.log("Esse é o original: " + `${original}`);
+    console.log("====================================")
+
+    //pega o número e soma com o resultado
+    return acumulador += numero;
+})
+
+console.log("Total do REDUCE: " + total);
