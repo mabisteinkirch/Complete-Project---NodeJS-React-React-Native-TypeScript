1bdf8c6e5b8a030f9a086a97255c84f01e554f40
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-14 11:09:49 -0300

    Feat: Using includes function in JavaScript

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-//função somar não anônima
 
-function somar (a, b){
-    let total = a + b;
-    return console.log (total);
+//INCLUDES 
 
-}
+let nomes = ["Mabi", "Lucas"];
 
-somar(10, 30);
-
-let anonymousFunction = (a, b) => {
-    let total = a + b;
-    console.log (total);
-}
-
-anonymousFunction(10,30);
-
-let numeros = [1,2,5,10];
-
-numeros.map ((item) => {
-    console.log(item);
-})
+console.log(nomes.includes("Mabi"));
