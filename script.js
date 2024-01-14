a02c877e2d15d43c34c04d41259fbdb8abad84b8
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-14 08:15:00 -0300

    Feat: Spread operator with array in JavaScript

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-//objeto
+//spread operator
 
-let pessoa = {
-    nome: "Mabi",
-    sobrenome: "von Steinkirch",
-    cargo: "Fullstack engineer",
-    altura: 1.59
-}
+let primeiros = [1,2,3];
 
-//acessar: pessoa.altura
+let numeros = [...primeiros, 4,5,10];
+console.log(numeros);
 
-//console.log (pessoa);
-//console.log (pessoa.nome);
-//console.log (pessoa.cargo);
-
-//desconstruir o array
-const { nome: rename, cargo } = pessoa;
-console.log (rename);
-console.log (cargo);
-
-let nomes = ["Mabi", "Lucas", "Mika"];
-
-//forma padrão de pegar algum item
-console.log(nomes[0]);
-
-//desconstruir para pegar mais que uma variável por vez
-
-let { 0:primeiro, 2:ultimo} = nomes;
-console.log(primeiro);
-console.log(ultimo);
-
-let [primeiroNome, segundoNome] = nomes;
