1752a5164b88a1525c14686c2b3935beb564b1ea
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-14 10:54:27 -0300

    Feat: anonymous function in JavaScript

--- script.js
+++ script.js
@@ -1,47 +1,16 @@
-//MAP - PERCORRER TODO UM ARRAY
+//função somar não anônima
 
-let lista = ["Mabi", "Lucas", "Nekochan"];
+function somar (a, b){
+    let total = a + b;
+    return console.log (total);
 
-lista.map((item, index) => {
-    console.log("Esse é o item: " + item + " Esse é o index: " + index);
-})
+}
 
-//REDUCE - BUSCA REDUZIR UM ARRAY
+somar(10, 30);
 
-let numeros =  [5,3,2];
-// somar todos os numeros
+let anonymousFunction = (a, b) => {
+    let total = a + b;
+    console.log (total);
+}
 
-let total = numeros.reduce((acumulador, numero, indice, original)=>{
-    console.log("Esse é o acumulador: " + `${acumulador}`);
-    console.log("Esse é o numero: " + `${numero}`);
-    console.log("Esse é o indice: " + `${indice}`);
-    console.log("Esse é o original: " + `${original}`);
-    console.log("====================================")
-
-    //pega o número e soma com o resultado
-    return acumulador += numero;
-})
-
-console.log("Total do REDUCE: " + total);
-
-// FIND - Devolve o primeiro item que ele encontrar
-
-let listagem = [5, 3, "Mabi", 2, "Gato"]
-
-let busca = listagem.find((item)=>{
- if (item === "Mabi")
-    return console.log ("Encontrado");
-})
-
-console.log(busca);
-
-//FILTER - saber quantos itens repetidos tem
-
-let nomes = ["Mabi", "Lucas", "Mabi", "Mabi", "Gato", "Lucas"]
-
-let resultado = nomes.filter((item)=>{
- if (item === "Lucas")
-    return item.length;
-})
-
-console.log(resultado);
+anonymousFunction(10,30);
