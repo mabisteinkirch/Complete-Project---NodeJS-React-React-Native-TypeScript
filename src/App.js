2b2f94965b0ba2c8c9a6531e6c23d582ed306b7d
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-21 08:38:46 -0300

    Feat: Using form in REACT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import {useState} from 'react';
-import Name from './components/Name';
 
 function App(){
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState({});
 
-  const [person, setPerson] = useState('Programmmer')
-
-  function handleChangeName(nome){
-    setPerson(nome);
+  function handleRegister (e){
+    e.preventDefault();
+    alert ('Usuário registrado com sucesso')
+    setUser({
+      name: name,
+      age: age,
+      email: email,
+    })
   }
 
   return(
     <div>
-      <h1>Componente App</h1>
-      <h2>Hello: {person}</h2>
-      <button onClick={ () => handleChangeName ('Mabi')}>
-        Change name
-      </button>
-      
+      <form onSubmit={handleRegister}>
+        <label>Name: </label>
+        <input placeholder="Digite seu nome" value={name} onChange={(e) => setName(e.target.value)}/> <br/>
+
+        <label>Age: </label>
+        <input placeholder="Digite sua idade" value={age} onChange={(e) => setAge(e.target.value)}/> <br/>
+
+        <label>Email: </label>
+        <input placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)}/> <br/>
+
+       <button type="submit">Register</button>
+      </form>
+      <br/><br/>
+      <div>
+        <span>You are welcome {user.name}</span><br/>
+        <span>Age: {user.age}</span><br/>
+        <span>Email:  {user.email}</span><br/>
+      </div> 
     </div>
   );
 }
-
 export default App;
 
