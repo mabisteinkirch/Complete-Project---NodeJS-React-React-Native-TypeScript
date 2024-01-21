0b83acc0c825d28e7eaa5a7dd1f94ff528aa88e1
Mabi Prux von Steinkirch <70056552+mabisteinkirch@users.noreply.github.com>
2024-01-21 06:47:17 -0300

    Feat: Use a local component within another component without needing to export

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ function App(){
   return(
     <div>
       <h1>Componente App</h1>
+      <Name/>
     </div>
   );
 }
 
 export default App;
+
+function Name(){
+  return(
+    <span>Hello Mabi</span>
+  )
+}
